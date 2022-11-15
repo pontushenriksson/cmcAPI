22e2e0d32ee3944e4afa635b299a6fbcf2ca0354
pontushenriksson <pontushenrikssn@gmail.com>
2022-11-15 23:44:56 +0100

    added bugs.md

---
+++ bugs.md
@@ -0,0 +1,3 @@
+**_Only accepts cmc ranking position to get coin info_**
+
+- fix: check coin ranking and then use that info to get the data but it requires 2 requests instead of 1.
--- cmcAPI.py
+++ cmcAPI.py
@@ -4,45 +4,93 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 apiKey = os.getenv('apiKey')
+coin = 0
+current_coin = 0
+amount_of_coins = 5
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
+for i in range (amount_of_coins):
+  current_coin += 1;
+  # Generates output of coin 1
+  if current_coin == 1:
+    coin = 1
+  # Generates output of coin 2
+  elif current_coin == 2:
+    coin = 2
+  # Generates output of coin 3
+  elif current_coin == 3:
+    coin = 3
+  # Generates output of coin 4
+  elif current_coin == 4:
+    coin = 4
+  # Generates output of coin 5
+  elif current_coin == 5:
+    coin = 10
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+  parameters = {
+    'start':coin,
+    'limit':'1',
+    'convert':'USD'
+  }
+  headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': apiKey,
+  }
+  session = Session()
+  session.headers.update(headers)
+  try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    print("\033[32m############## All data ##############\033[0m")
+    print(data)
+    print("\033[32m#######################################\033[0m")
+  except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+  # To write the data to the file, we just call the dump() function from json library
+  with open('response.json', 'w') as json_file:
+      json.dump(data, json_file)
+  with open("response.json", "r") as f:
+      my_dict = json.load(f)
+      # Saves better variables
+      if current_coin == 1:
+        coin1_price = my_dict["data"][0]["quote"]["USD"]["price"]
+        coin1_cmcRank = my_dict["data"][0]["cmc_rank"]
+      elif current_coin == 2:
+        coin2_price = my_dict["data"][0]["quote"]["USD"]["price"]
+        coin2_cmcRank = my_dict["data"][0]["cmc_rank"]
+      elif current_coin == 3:
+        coin3_price = my_dict["data"][0]["quote"]["USD"]["price"]
+        coin3_cmcRank = my_dict["data"][0]["cmc_rank"]
+      elif current_coin == 4:
+        coin4_price = my_dict["data"][0]["quote"]["USD"]["price"]
+        coin4_cmcRank = my_dict["data"][0]["cmc_rank"]
+      elif current_coin == 5:
+        coin5_price = my_dict["data"][0]["quote"]["USD"]["price"]
+        coin5_cmcRank = my_dict["data"][0]["cmc_rank"]
 
-  'start':'1',
-  'limit':'1',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': apiKey,
-}
+for i in range (amount_of_coins):
+  GenerateData()
 
-session = Session()
-session.headers.update(headers)
-
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print("\033[32m############## All data ##############\033[0m")
-  print(data)
-  print("\033[32m#######################################\033[0m")
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
-
-# To write the data to the file, we just call the dump() function from json library
-with open('response.json', 'w') as json_file:
-    json.dump(data, json_file)
-
-with open("response.json", "r") as f:
-    my_dict = json.load(f)
-    price = my_dict["data"][0]["quote"]["USD"]["price"]
-    cmcRank = my_dict["data"][0]["cmc_rank"]
-
-    print("\033[34m############## Specific data ##############\033[0m")
-    print("\033[34mCurrent price: \033[0m", price)
-    print("\033[34mCurrent cmc rank: \033[0m", cmcRank)
-    print("\033[34m###########################################\033[0m")
+print("\033[34m############## Specific data ##############\033[0m")
+print("\033[34mCoin 1:\033[0m")
+print("\033[34mCurrent price: \033[0m", coin1_price)
+print("\033[34mCurrent cmc rank: \033[0m", coin1_cmcRank)
+print("\033[34m---\033[0m")
+print("\033[34mCoin 2:\033[0m")
+print("\033[34mCurrent price: \033[0m", coin2_price)
+print("\033[34mCurrent cmc rank: \033[0m", coin2_cmcRank)
+print("\033[34m---\033[0m")
+print("\033[34mCoin 3:\033[0m")
+print("\033[34mCurrent price: \033[0m", coin3_price)
+print("\033[34mCurrent cmc rank: \033[0m", coin3_cmcRank)
+print("\033[34m---\033[0m")
+print("\033[34mCoin 4:\033[0m")
+print("\033[34mCurrent price: \033[0m", coin4_price)
+print("\033[34mCurrent cmc rank: \033[0m", coin4_cmcRank)
+print("\033[34m---\033[0m")
+print("\033[34mCoin 5:\033[0m")
+print("\033[34mCurrent price: \033[0m", coin5_price)
+print("\033[34mCurrent cmc rank: \033[0m", coin5_cmcRank)
+print("\033[34m###########################################\033[0m")
