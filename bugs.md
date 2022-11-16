fc13e7d6dd77cdeea3f1801d86920f3d201d8304
pontushenriksson <pontushenrikssn@gmail.com>
2022-11-16 01:14:23 +0100

    fixed bug and added more data

--- bugs.md
+++ bugs.md
@@ -1,3 +1,1 @@
-**_Only accepts cmc ranking position to get coin info_**
-
-- fix: check coin ranking and then use that info to get the data but it requires 2 requests instead of 1.
+**_There are currentlyno bugs! :D_**
--- cmcAPI.py
+++ cmcAPI.py
@@ -7,90 +7,98 @@ from dotenv import load_dotenv
 load_dotenv()
 
 apiKey = os.getenv('apiKey')
-coin = 0
-current_coin = 0
-amount_of_coins = 5
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=XMR,ETH,DOGE,BTC,RVN'
 
-for i in range (amount_of_coins):
-  current_coin += 1;
-  # Generates output of coin 1
-  if current_coin == 1:
-    coin = 1
-  # Generates output of coin 2
-  elif current_coin == 2:
-    coin = 2
-  # Generates output of coin 3
-  elif current_coin == 3:
-    coin = 3
-  # Generates output of coin 4
-  elif current_coin == 4:
-    coin = 4
-  # Generates output of coin 5
-  elif current_coin == 5:
-    coin = 10
-  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-  parameters = {
-    'start':coin,
-    'limit':'1',
-    'convert':'USD'
-  }
-  headers = {
+parameters = {
+}
+headers = {
     'Accepts': 'application/json',
     'X-CMC_PRO_API_KEY': apiKey,
-  }
-  session = Session()
-  session.headers.update(headers)
-  try:
-    response = session.get(url, params=parameters)
-    data = json.loads(response.text)
-    print("\033[32m############## All data ##############\033[0m")
-    print(data)
-    print("\033[32m#######################################\033[0m")
-  except (ConnectionError, Timeout, TooManyRedirects) as e:
-    print(e)
-  # To write the data to the file, we just call the dump() function from json library
-  with open('response.json', 'w') as json_file:
-      json.dump(data, json_file)
-  with open("response.json", "r") as f:
-      my_dict = json.load(f)
-      # Saves better variables
-      if current_coin == 1:
-        coin1_price = my_dict["data"][0]["quote"]["USD"]["price"]
-        coin1_cmcRank = my_dict["data"][0]["cmc_rank"]
-      elif current_coin == 2:
-        coin2_price = my_dict["data"][0]["quote"]["USD"]["price"]
-        coin2_cmcRank = my_dict["data"][0]["cmc_rank"]
-      elif current_coin == 3:
-        coin3_price = my_dict["data"][0]["quote"]["USD"]["price"]
-        coin3_cmcRank = my_dict["data"][0]["cmc_rank"]
-      elif current_coin == 4:
-        coin4_price = my_dict["data"][0]["quote"]["USD"]["price"]
-        coin4_cmcRank = my_dict["data"][0]["cmc_rank"]
-      elif current_coin == 5:
-        coin5_price = my_dict["data"][0]["quote"]["USD"]["price"]
-        coin5_cmcRank = my_dict["data"][0]["cmc_rank"]
+}
+session = Session()
+session.headers.update(headers)
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print("\033[32m############## All data ##############\033[0m")
+  print(data)
+  print("\033[32m#######################################\033[0m")
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+    
+# To write the data to the file, we just call the dump() function from json library
+with open('response.json', 'w') as json_file:
+  json.dump(data, json_file)
+with open("response.json", "r") as f:
+  my_dict = json.load(f)
 
-for i in range (amount_of_coins):
-  GenerateData()
+# Saves better variables
+BTC_name = my_dict["data"]["BTC"]["name"]
+BTC_symbol = my_dict["data"]["BTC"]["symbol"]
+BTC_price = my_dict["data"]["BTC"]["quote"]["USD"]["price"]
+BTC_cmcRank = my_dict["data"]["BTC"]["cmc_rank"]
+BTC_market_dominance = my_dict["data"]["BTC"]["quote"]["USD"]["market_cap_dominance"]
+
+DOGE_name = my_dict["data"]["DOGE"]["name"]
+DOGE_symbol = my_dict["data"]["DOGE"]["symbol"]
+DOGE_price = my_dict["data"]["DOGE"]["quote"]["USD"]["price"]
+DOGE_cmcRank = my_dict["data"]["DOGE"]["cmc_rank"]
+DOGE_market_dominance = my_dict["data"]["DOGE"]["quote"]["USD"]["market_cap_dominance"]
+
+ETH_name = my_dict["data"]["ETH"]["name"]
+ETH_symbol = my_dict["data"]["ETH"]["symbol"]
+ETH_price = my_dict["data"]["ETH"]["quote"]["USD"]["price"]
+ETH_cmcRank = my_dict["data"]["ETH"]["cmc_rank"]
+ETH_market_dominance = my_dict["data"]["ETH"]["quote"]["USD"]["market_cap_dominance"]
+
+RVN_name = my_dict["data"]["RVN"]["name"]
+RVN_symbol = my_dict["data"]["RVN"]["symbol"]
+RVN_price = my_dict["data"]["RVN"]["quote"]["USD"]["price"]
+RVN_cmcRank = my_dict["data"]["RVN"]["cmc_rank"]
+RVN_market_dominance = my_dict["data"]["RVN"]["quote"]["USD"]["market_cap_dominance"]
+
+XMR_name = my_dict["data"]["XMR"]["name"]
+XMR_symbol = my_dict["data"]["XMR"]["symbol"]
+XMR_price = my_dict["data"]["XMR"]["quote"]["USD"]["price"]
+XMR_cmcRank = my_dict["data"]["XMR"]["cmc_rank"]
+XMR_market_dominance = my_dict["data"]["XMR"]["quote"]["USD"]["market_cap_dominance"]
 
 print("\033[34m############## Specific data ##############\033[0m")
-print("\033[34mCoin 1:\033[0m")
-print("\033[34mCurrent price: \033[0m", coin1_price)
-print("\033[34mCurrent cmc rank: \033[0m", coin1_cmcRank)
+print("\033[34mToken name: \033[0m", BTC_name)
+print("\033[34mToken symbol: \033[0m", BTC_symbol)
+print("\033[34mCurrent price: \033[0m", BTC_price,"$ USD per token")
+print("\033[34mCurrent cmc rank: \033[0m", BTC_cmcRank)
+print("\033[34mCurrent cmc rank: \033[0m", BTC_market_dominance,"%")
+
 print("\033[34m---\033[0m")
-print("\033[34mCoin 2:\033[0m")
-print("\033[34mCurrent price: \033[0m", coin2_price)
-print("\033[34mCurrent cmc rank: \033[0m", coin2_cmcRank)
+
+print("\033[34mToken name: \033[0m", DOGE_name)
+print("\033[34mToken symbol: \033[0m", DOGE_symbol)
+print("\033[34mCurrent price: \033[0m", DOGE_price,"$ USD per token")
+print("\033[34mCurrent cmc rank: \033[0m", DOGE_cmcRank)
+print("\033[34mCurrent cmc rank: \033[0m", DOGE_market_dominance,"%")
+
 print("\033[34m---\033[0m")
-print("\033[34mCoin 3:\033[0m")
-print("\033[34mCurrent price: \033[0m", coin3_price)
-print("\033[34mCurrent cmc rank: \033[0m", coin3_cmcRank)
+
+print("\033[34mToken name: \033[0m", ETH_name)
+print("\033[34mToken symbol: \033[0m", ETH_symbol)
+print("\033[34mCurrent price: \033[0m", ETH_price,"$ USD per token")
+print("\033[34mCurrent cmc rank: \033[0m", ETH_cmcRank)
+print("\033[34mCurrent cmc rank: \033[0m", ETH_market_dominance,"%")
+
 print("\033[34m---\033[0m")
-print("\033[34mCoin 4:\033[0m")
-print("\033[34mCurrent price: \033[0m", coin4_price)
-print("\033[34mCurrent cmc rank: \033[0m", coin4_cmcRank)
+
+print("\033[34mToken name: \033[0m", RVN_name)
+print("\033[34mToken symbol: \033[0m", RVN_symbol)
+print("\033[34mCurrent price: \033[0m", RVN_price,"$ USD per token")
+print("\033[34mCurrent cmc rank: \033[0m", RVN_cmcRank)
+print("\033[34mCurrent cmc rank: \033[0m", RVN_market_dominance,"%")
+
 print("\033[34m---\033[0m")
-print("\033[34mCoin 5:\033[0m")
-print("\033[34mCurrent price: \033[0m", coin5_price)
-print("\033[34mCurrent cmc rank: \033[0m", coin5_cmcRank)
+
+print("\033[34mToken name: \033[0m", XMR_name)
+print("\033[34mToken symbol: \033[0m", XMR_symbol)
+print("\033[34mCurrent price: \033[0m", XMR_price,"$ USD per token")
+print("\033[34mCurrent cmc rank: \033[0m", XMR_cmcRank)
+print("\033[34mCurrent cmc rank: \033[0m", XMR_market_dominance,"%")
 print("\033[34m###########################################\033[0m")
